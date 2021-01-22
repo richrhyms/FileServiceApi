1e4d3df7994d129e5cc053095400257fe6f081ff
rotaru <rotaru@byteworks.com.ng>
2021-01-22 14:16:20 +0100

    setting up spring boot

---
+++ src/main/java/com/richrhyms/fileserviceapi/FileServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.richrhyms.fileserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richrhyms/fileserviceapi/FileServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richrhyms.fileserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
